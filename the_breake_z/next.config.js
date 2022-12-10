eb6264d0e3b48a0b9bdd806f535f620d855f9e58
Jeon JongChan <loki377312@gmail.com>
2022-12-11 03:21:38 +0900

    CORS 극복하기 1차

--- the_breake_z/next.config.js
+++ the_breake_z/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+let host = process.env.NEXT_PUBLIC_HOST;
 const nextConfig = {
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: "/:path*",
+                destination: host + `/:path*`,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;

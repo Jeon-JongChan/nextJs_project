2c76ae3896a7885b373437f9c43c2689744113b0
Jeon JongChan <loki377312@gmail.com>
2022-12-11 04:13:38 +0900

    CORS 4

--- the_breake_z/pages/api/data.js
+++ the_breake_z/pages/api/data.js
@@ -6,11 +6,10 @@ export default function handler(req, res) {
     let ret = null;
     let method = req.method;
 
-    // nextjs CORS allow
-    res.setHeader("Access-Control-Allow-Credentials", true);
+    // nextjs CORS allow for http://localhost:3000
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     //ret = server.db.prepare("select count(*) cnt from spec").get();
     try {

07bce7b47c416a23834d63c9f72b4ba7edcd2b2f
Jeon Jongchan <loki377312@gmail.com>
2023-02-18 15:09:52 +0900

    powershell 변수 추가

---
+++ ticketmacro/start.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+ECHO "*********************************************>> THE BREAK Z START BAT"
+
+@REM powershell version
+@REM $env:PATH += ";" + (Join-Path (Get-Location) "node") 
+SET NODEJS=%cd%/node
+SET PATH=%NODEJS%;%PATH%
+
+IF EXIST %cd%/node_modules (
+    ECHO "*********************************************>> NODE_MODULES EXIST. START SUCCESS" 
+) ELSE (
+    ECHO "*********************************************>> NODE_MODULES NOT EXIST. INSTALL PROCESS. ENTER key please"
+    %cd%/node/npm i
+)
+%cd%/node/npm run dev

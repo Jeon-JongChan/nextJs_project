04f869ed042169d128e32c725d7c67131077c696
Jeon JongChan <loki377312@gmail.com>
2024-03-11 18:59:11 +0900

    인증오류 - 8차

--- the_breake_z/next.config.js
+++ the_breake_z/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 // const external_host = process.env.NEXT_PUBLIC_EXTERNAL_HOST;
 // const host = process.env.NEXT_PUBLIC_HOST;
-const domain = process.env.NEXT_PUBLIC_HOST;
+const domain = process.env.NEXT_PUBLIC_DOMAIN;
 const nextConfig = {
   reactStrictMode: true,
   images: {

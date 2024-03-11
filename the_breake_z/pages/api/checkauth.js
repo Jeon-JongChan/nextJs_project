4b2d39b852b7e1d7a27bdfe7115c335fd3f1fec2
Jeon JongChan <loki377312@gmail.com>
2024-03-11 18:48:34 +0900

    인증-4차문제 : 쿠키저장안됨

--- the_breake_z/pages/api/checkauth.js
+++ the_breake_z/pages/api/checkauth.js
@@ -5,6 +5,9 @@ export default function handler(req, res) {
     res.status(401).json({error: "Invalid key"});
     return;
   }
+  res.setHeader("Access-Control-Allow-Origin", "*"); // 모든 출처 허용
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // 허용하는 HTTP 메소드 목록
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // 허용하는 요청 헤더 목록
   res.setHeader("Set-Cookie", "authkey=" + req.body.key + "; max-age=86400; Path=/; HttpOnly; Secure; SameSite=Strict");
   res.status(200).json({name: "John Doe"});
 }

5f1f9eebd55340f888da1c1c162c6b9325ecd6fa
Jeon JongChan <loki377312@gmail.com>
2024-03-11 19:06:34 +0900

    인증오류 - 10차

--- the_breake_z/pages/api/checkauth.js
+++ the_breake_z/pages/api/checkauth.js
@@ -8,6 +8,6 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*"); // 모든 출처 허용
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // 허용하는 HTTP 메소드 목록
   res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // 허용하는 요청 헤더 목록
-  res.setHeader("Set-Cookie", "authkey=" + req.body.key + "; max-age=86400; Path=/; HttpOnly; Secure");
+  res.setHeader("Set-Cookie", "authkey=" + req.body.key + "; max-age=86400;");
   res.status(200).json({name: "John Doe"});
 }

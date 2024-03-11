d1da1f520635fc5a9268fa991cfc43da9001a1e4
Jeon JongChan <loki377312@gmail.com>
2024-03-11 18:45:12 +0900

    인증 - 3차 오류 수정, env설정 수정

--- the_breake_z/middleware.js
+++ the_breake_z/middleware.js
@@ -2,7 +2,7 @@ import {NextResponse} from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  let host = process.env.NEXT_PUBLIC_DOMAIN;
+  let host = process.env.NEXT_PUBLIC_HOST;
   let admin = process.env.NEXT_ADMIN;
   let users = [process.env.NEXT_ADMIN || "qwer", process.env.NEXT_USER1 || "qwer1", process.env.NEXT_USER2 || "qwer2", process.env.NEXT_USER3 || "qwer3"];
   let authkey = request.cookies.get("authkey");

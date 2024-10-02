ce568d760b0a7b67fb2f1440d49c71c2a3a47098
Jeon Jongchan <loki377312@gmail.com>
2024-10-02 18:46:17 +0900

    build 버그 수정

--- web_shineseeker/app/(admin)/admin-manager/AdminTabPatrol.js
+++ web_shineseeker/app/(admin)/admin-manager/AdminTabPatrol.js
@@ -141,7 +141,12 @@ export default function Home() {
         </div>
       </div>
       <div className={`w-4/5 flex flex-col ${menuName}-form`}>
-        <form onSubmit={handleSubmitUser} data-apitype={`update_${menuName}`} className="grid grid-cols-12 gap-1 shadow sm:overflow-hidden sm:rounded-md p-4 bg-slate-100 w-full" style={{minHeight: "400px"}}>
+        <form
+          onSubmit={handleSubmitUser}
+          data-apitype={`update_${menuName}`}
+          className="grid grid-cols-12 gap-1 shadow sm:overflow-hidden sm:rounded-md p-4 bg-slate-100 w-full"
+          style={{minHeight: "400px"}}
+        >
           <div className="relative col-span-12 mt-4 flex gap-1">
             <div className="block w-1/4">
               <label htmlFor="patrol_img" className="block text-2xl font-bold">
@@ -175,7 +180,7 @@ export default function Home() {
             let option = index === 0 ? {type: "종류", money: "재화", count: "개수", img: "출력이미지", msg: "메세지"} : {};
             return <React.Fragment key={index}>{makeRetOptionGenerator(index, option)}</React.Fragment>;
           })}
-          <span className="col-span-1 relative row flex items-end justify-center font-bold text-red-500">"조건미달</span>
+          <span className="col-span-1 relative row flex items-end justify-center font-bold text-red-500">조건미달</span>
           <GridInputSelectBox id={`patrol_fail_type`} type={"text"} colSpan={2} options={patrolDefaultList.patrol_option_item} />
           <GridInputText id={`patrol_fail_money`} type={"text"} colSpan={2} css="border-b h-[36px]" />
           <GridInputText id={`patrol_fail_count`} type={"text"} colSpan={2} css="border-b h-[36px]" />
@@ -202,7 +207,9 @@ export default function Home() {
 function makeRetOptionGenerator(index, option = {}) {
   return (
     <React.Fragment key={index}>
-      <span className={["col-span-1 relative row flex items-end justify-center font-bold", option?.spanCss ? option?.spanCss : ""].join(" ")}>{option.span ? option?.span : `${index + 1} 선택지`}</span>
+      <span className={["col-span-1 relative row flex items-end justify-center font-bold", option?.spanCss ? option?.spanCss : ""].join(" ")}>
+        {option.span ? option?.span : `${index + 1} 선택지`}
+      </span>
       <GridInputSelectBox label={option?.type} id={`patrol_ret_type_${index}`} type={"text"} colSpan={2} options={patrolDefaultList.patrol_option_item} />
       <GridInputText label={option?.money} id={`patrol_ret_money_${index}`} type={"text"} colSpan={2} css="border-b h-[36px]" />
       <GridInputText label={option?.count} id={`patrol_ret_count_${index}`} type={"text"} colSpan={2} css="border-b h-[36px]" />
